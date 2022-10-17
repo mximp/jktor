fc8447d3560207798da082d0644010d550bf3e68
mximp <mxim.petrov@gmail.com>
2022-10-17 08:10:20 +0300

    Classifier interface

--- src/main/java/org/sct/jktor/Classifier.java
+++ src/main/java/org/sct/jktor/Classifier.java
@@ -1,4 +1,28 @@
 package org.sct.jktor;
 
+/**
+ * Classifier interface.
+ *
+ * @since 1.0
+ * @author Maxim Petrov
+ */
 public interface Classifier {
+
+    /**
+     * Name.
+     * @return A string with name
+     */
+    String name();
+
+    /**
+     * Depth of classifier path.
+     * @return Depth
+     */
+    int depth();
+
+    /**
+     * Get parent of this.
+     * @return Parent {@link Classifier}
+     */
+    Classifier parent();
 }
